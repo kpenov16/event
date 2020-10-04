bd2117c30187537cde9e419ffb6c87b7eae98ad5
kpenov16 <s133967@student.dtu.dk>
2020-10-04 23:24:16 +0200

    add mysql docker container for db auth

--- src/test/java/dk/kaloyan/event/EventApplicationTests.java
+++ src/test/java/dk/kaloyan/event/EventApplicationTests.java
@@ -1,13 +1,17 @@
 package dk.kaloyan.event;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class EventApplicationTests {
+	@Autowired private PasswordEncoder encoder;
 
 	@Test
 	void contextLoads() {
+		System.out.println(encoder.encode("pass"));
 	}
 
 }
